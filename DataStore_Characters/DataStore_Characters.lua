0684bbc1661b54f3ea7f32b34dd4b44dd28f9ce5
GovtGeek <26489473+GovtGeek@users.noreply.github.com>
2026-04-08 00:06:08 -0700

    Make MAX_LEVEL_INTERNAL more dynamic

--- DataStore_Characters/DataStore_Characters.lua
+++ DataStore_Characters/DataStore_Characters.lua
@@ -21,7 +21,7 @@ local bAnd = bit.band
 local bOr = bit.bor
 
 -- MAX_PLAYER_LEVEL is provided in MoP, but nil in retail
-local MAX_LEVEL_INTERNAL = MAX_PLAYER_LEVEL or 80
+local MAX_LEVEL_INTERNAL = MAX_PLAYER_LEVEL or GetMaxLevelForPlayerExpansion()
 
 local MAX_LOGOUT_TIMESTAMP = 5000000000	-- 5 billion, current values are at ~1.4 billion, in seconds, that leaves us 110+ years, I think we're covered..
 local MAX_ALT_LEVEL = WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC
